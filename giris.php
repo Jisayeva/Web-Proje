100954089a4e19330d22c211535d7ec9de2e573c
Zhala Isayeva <jisayeva6@gmail.com>
2025-05-24 21:25:56 +0300

    giris sayfasi yapildi

--- giris.php
+++ giris.php
@@ -2,13 +2,13 @@
 $email = $_POST['email']??'';
 $sifre = $_POST['password']??'';
 
-$dogruEmail = "b241210001@sakarya.edu.tr";
-$dogruSifre = "b241210001";
+$dogruEmail = "b241210563@sakarya.edu.tr";
+$dogruSifre = "b241210563";
 
 if ($email === $dogruEmail && $sifre === $dogruSifre) {
-    echo "<h2 style='text-align:center; margin-top:50px;'>Hoş geldiniz <strong>" .$sifre."</strong><h2>";
-} eles {
-    header("Location: giris.html");
+    echo "<h2 style='text-align:center; margin-top:50px;'>Hoş geldiniz <strong>" .$sifre."</strong></h2>";
+} else {
+    header("Location: giris.html?error=1");
     exit();
 }
 ?>
